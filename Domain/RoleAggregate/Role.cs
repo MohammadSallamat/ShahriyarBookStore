4d8a618a91dcca3bacf69007b1e95057f3db2044
mohammad-81 <mmdsallamat2002@gmail.com>
2025-10-27 23:34:24 +0330

    AddRoleAggregate

---
+++ Domain/RoleAggregate/Role.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Common.Domain;
+using Domain.Common.Domain.Extention;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.RoleAggregate;
+
+public class Role : AggregateRoot
+{
+    public string Title { get; private set; }
+    public List<RolePermission> Permissions { get; private set; }
+
+    public Role(string title, List<RolePermission> permissions)
+    {
+        NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+
+        Title = title;
+        Permissions = permissions;
+    }
+
+    public void EditTitle(string title)
+    {
+        NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+        Title = title;
+    }
+
+    public void SetPermission(List<RolePermission> permissions)
+    {
+        Permissions = permissions;
+    }
+}
---
+++ Domain/RoleAggregate/RolePermission.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common.Domain;
+using Domain.RoleAggregate.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.RoleAggregate;
+
+public class RolePermission : BaseEntity
+{
+
+    public long RoleId { get; internal set; }
+    public Permission Permission { get; private set; }
+}
